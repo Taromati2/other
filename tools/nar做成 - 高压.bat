372c05bdc3aaec346acefbdf2543a9cd500b31be
steve02081504 <steve02081504@EDEN.fukwold>
2022-07-25 03:09:37 +0800

    fix 7z flag

--- tools/nar做成 - 高压.bat
+++ tools/nar做成 - 高压.bat
@@ -3,5 +3,5 @@ echo nar×ö³ÉÖÐ........
 set "ZIP=E:\7-Zip\7z.exe"
 set "ARGS=%*"
 setlocal EnableDelayedExpansion
-for %%x in (!ARGS!) do "%ZIP%" a -tzip "%%~dpx%%~nxx.nar" -mx=9 -mmt -mm=LZMA -md=1536m -mfb=273 -mtc=off -r "%%~dpx%%~nxx"
+for %%x in (!ARGS!) do "%ZIP%" a -tzip "%%~dpx%%~nxx.nar" -mx=9 -mmt -mm=LZMA -md=1536m -mfb=273 -mtc=off "%%~dpx%%~nxx"
 pause
